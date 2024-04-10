4b6de7ea9c8328f698672a8534288b941a6df51b
Kaleab Mulugeta <kalmulmath@gmail.com>
2024-04-10 10:18:56 -0700

    test

---
+++ 0x01-variables_if_else_while/10-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			if (i == j)
+				continue;
+			putchar(i + 48);
+			putchar(j + 48);
+			if (i == 8 && j == 9)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
