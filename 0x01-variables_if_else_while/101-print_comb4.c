1a5f605ce54da06d6309659292488a35560a63e2
Kaleab Mulugeta <kalmulmath@gmail.com>
2024-04-10 10:26:41 -0700

    task finished

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar(i + 48);
+				putchar(j + 48);
+				putchar(k + 48);
+				if (i == 7 && j == 8 && k == 9)
+					break;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
