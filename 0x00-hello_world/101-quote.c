8ab4310a60fc623d6198f6fb8326cd4e1c4eedbb
Kaleab Mulugeta <kalmulmath@gmail.com>
2024-04-10 04:37:57 -0700

    problem solved

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Return: ALways 1
+ */
+int main(void)
+{
+	char str[59] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int i = 0;
+	char c = '\n';
+	while ( str[i] != '\0' )
+	{
+		write(STDOUT_FILENO, &str[i], 1);
+		i++;
+	}
+	write(STDOUT_FILENO, &c, 1);
+	return (1);
+}
