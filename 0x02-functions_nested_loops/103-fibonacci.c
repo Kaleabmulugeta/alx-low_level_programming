d0a12c5ccf3e604c23569ff95d338e4566609c52
Kaleab Mulugeta <kalmulmath@gmail.com>
2024-04-13 06:23:24 -0700

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * fibonacci - check the code
+ *
+ * Return: Always 0.
+ */
+void fibonacci(void)
+{
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+	unsigned long tot = 0;
+
+	while (1)
+	{
+		sum = fib1 + fib2;
+		if (sum > 4000000)
+			break;
+		if (sum % 2 == 0)
+                        tot += sum;
+		fib1 = fib2;
+		fib2 = sum;
+	}
+	printf("%li\n", tot);
+}
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	fibonacci();
+	return (0);
+}
