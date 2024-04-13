9a55ab8a3b071823851674bede693fdaff751c68
Kaleab Mulugeta <kalmulmath@gmail.com>
2024-04-13 06:28:03 -0700

    task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * fibonacci - check the code
+ *
+ * Return: Always 0.
+ */
+void fibonacci(void)
+{
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
+
+	for (count = 0; count < 98; count++)
+	{
+		sum = fib1 + fib2;
+		printf("%lu", sum);
+
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 97)
+			printf("\n");
+		else
+			printf(", ");
+	}
+}
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	fibonacci();
+	return (0);
+}
