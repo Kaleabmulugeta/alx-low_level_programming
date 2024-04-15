9af2b6f3cb0b93653312ce3416b0bd5c69cfcc40
Kaleab Mulugeta <kalmulmath@gmail.com>
2024-04-15 03:47:15 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints all numbers from 0 to 9
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 15; i++)
+	{
+		if (i > 9)
+			_putchar(i / 10 + 48);
+		_putchar(i % 10 + 48);
+	}
+	_putchar('\n');
+}
