6ad02ce05b55a7a2481a99db5a6814d28b0c391e
Kaleab Mulugeta <kalmulmath@gmail.com>
2024-04-15 02:51:14 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN
+#define MAIN
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ *
+ * print_alphabet - print all letters.
+ *
+ * _islower - checks if a char is lowercase
+ * _isalpha - checks if a char is a letter
+ * print_sign - checks the sign of a num
+ * _abs - absolute val
+ * times_table - prints the time table 0 - 9
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+
+#endif
