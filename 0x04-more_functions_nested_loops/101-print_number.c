3e4de14c02c7be4baf52ed696d3f3bf8fd6e6bd5
Kaleab Mulugeta <kalmulmath@gmail.com>
2024-04-15 06:44:12 -0700

    task 20

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_number - prints a num
+ * @n: int
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		print_number(-1 * n);
+		return;
+	}
+	if (n < 10)
+	{
+		_putchar(n + 48);
+		return;
+	}
+	else if (n < 100)
+	{
+		_putchar(n / 10 + 48);
+		_putchar(n % 10 + 48);
+		return;
+	}
+	else if (n < 1000)
+	{
+		_putchar(n / 100 + 48);
+		_putchar(((n / 10) - (n / 100) * 10) + 48);
+		_putchar(n % 10 + 48);
+		return;
+	}
+	else if (n < 10000)
+	{
+		_putchar(n / 1000 + 48);
+		_putchar(((n / 100) - (n / 1000) * 10) + 48);
+		_putchar(((n / 10) - (n / 100) * 10) + 48);
+		_putchar(n % 10 + 48);
+		return;
+	}
+}
