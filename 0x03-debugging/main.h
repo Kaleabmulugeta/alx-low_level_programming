28fe4e40588e6026e5a4cccdba3a59df721b69fc
Kaleab Mulugeta <kalmulmath@gmail.com>
2024-04-17 05:58:13 -0700

    Task 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,14 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * largest_number - finds the largest number from a triplet
- * @a: int 1
- * @b: int 2
- * @c: int 3
- *
- * Return: the largest number
- */
+#include <stdio.h>
+
 int largest_number(int a, int b, int c);
 
 #endif
