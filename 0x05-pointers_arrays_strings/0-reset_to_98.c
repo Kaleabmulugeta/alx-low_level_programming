2882d6743d6fea4748a79f648e071abb6bf5f706
Kaleab Mulugeta <kalmulmath@gmail.com>
2024-04-19 00:26:02 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets an int to 98
+ * @n: pointer to the integer that needs to be reset
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
