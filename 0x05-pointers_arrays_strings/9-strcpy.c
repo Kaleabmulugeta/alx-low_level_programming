4ce535f70b3736a4d36891fb0de829aa2a0b6144
Kaleab Mulugeta <kalmulmath@gmail.com>
2024-04-21 01:49:57 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,22 +1,4 @@
 /**
- * len - Gets the lenght of a str
- * @s: pointer of a ste
- *
- * Return: len
- */
-int len(char *s)
-{
-	int i = 0;
-
-	while (*s != '\0')
-	{
-		i++;
-		s++;
-	}
-	return (i);
-}
-
-/**
  * _strcpy - copies a str to another one
  * @dest: Destination
  * @src: source str
@@ -25,13 +7,15 @@ int len(char *s)
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-	int j = len(src);
+	char *src_p = src;
+	char *dest_p = dest;
 
-	while (i < j && src[i] != '\0')
+	while (*src_p != '\0')
 	{
-		dest[i] = src[i];
-		i++;
+		*dest_p = *src_p;
+		dest_p++;
+		src_p++;
 	}
+	*dest_p = '\0';
 	return (dest);
 }
