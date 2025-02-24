f6d229c5e234f84f84715ac917e7122d0b2531ff
Kaleab <kaeabmulugeta@hotmail.com>
2025-02-24 08:54:09 -0800

    styling

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,52 +9,51 @@
 void print_times_table(int n)
 {
 	int sp = 3;
-        int inc = 0;
-        int i;
-        int j;
-        int num = 0;
-        int dig;
-        
-	if (n < 0 || n > 15)
-                return;
+	int inc = 0;
+	int i;
+	int j;
+	int num = 0;
+	int dig;
 
-        for (i = 0; i <= n; i++)
-        {
-                sp = 3;
-                num = 0;
-                for (j = 0; j <= n; j++)
-                {
-                        if (num > 99)
-                        {
-                                sp = 1;
-                                _putchar((num / 100) + 48);
-                        }
-                        if (num > 9)
-                        {
-                                if (num < 99)
-                                        sp = 2;
-                                dig = (num / 10) - ((num / 100) * 10);
-                                _putchar(dig + 48);
-                        }
-                        _putchar((num % 10) + 48);
-                        if (j != n)
-                        {
-                                _putchar(',');
-                                if (sp == 3)
-                                {
-                                        _putchar(' ');
-                                        _putchar(' ');
-                                }
-                                if (sp == 2)
+	if (n < 0 || n > 15)
+		return;
+
+	for (i = 0; i <= n; i++)
+	{
+		sp = 3;
+		num = 0;
+		for (j = 0; j <= n; j++)
+		{
+			if (num > 99)
+			{
+				sp = 1;
+				_putchar((num / 100) + 48);
+			}
+			if (num > 9)
+			{
+				if (num < 99)
+					sp = 2;
+				dig = (num / 10) - ((num / 100) * 10);
+				_putchar(dig + 48);
+			}
+			_putchar((num % 10) + 48);
+			if (j != n)
+			{
+				_putchar(',');
+				if (sp == 3)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
 				if (sp == 2)
-                                {
-                                        _putchar(' ');
-                                }
-                                _putchar(' ');
-                        }
-                        num += inc;
-                }
-                _putchar('\n');
-                inc++;
-        }
+				{
+					_putchar(' ');
+				}
+					_putchar(' ');
+			}
+			num += inc;
+		}
+		_putchar('\n');
+		inc++;
+	}
 }
