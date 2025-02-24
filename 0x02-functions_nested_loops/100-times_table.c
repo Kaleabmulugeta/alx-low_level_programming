730131ee062c06085c36ae43406757def5192179
Kaleab <kaeabmulugeta@hotmail.com>
2025-02-24 10:01:20 -0800

    betty styling

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,64 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the times table 0 through 9
- *@n: int
- *
- * Return: Always 0.
+ * print_number - Prints a number with proper formatting.
+ * @num: The number to print.
+ */
+void print_number(int num)
+{
+	int dig;
+
+	if (num > 99)
+		_putchar((num / 100) + '0');
+	if (num > 9)
+	{
+		dig = (num / 10) % 10;
+		_putchar(dig + '0');
+	}
+	_putchar((num % 10) + '0');
+}
+
+/**
+ * print_separator - Prints separators between numbers.
+ * @num: The next number in the table.
+ * @inc: The increment value.
+ */
+void print_separator(int num, int inc)
+{
+	_putchar(',');
+	_putchar(' ');
+	if ((num + inc) < 10)
+	{
+		_putchar(' ');
+		_putchar(' ');
+	}
+	else if ((num + inc) < 100)
+		_putchar(' ');
+}
+
+/**
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: The number defining the table size (0 ≤ n ≤ 15).
  */
 void print_times_table(int n)
 {
-	int inc = 0;
-	int i;
-	int j;
-	int num = 0;
-	int dig;
+	int inc = 0, i, j, num;
+
 	if (n < 0 || n > 15)
 		return;
+
 	for (i = 0; i <= n; i++)
 	{
 		num = 0;
 		for (j = 0; j <= n; j++)
 		{
-			if (num > 99)
-			{
-				_putchar((num / 100) + 48);
-			}
-			if (num > 9)
-			{
-				dig = (num / 10) - ((num / 100) * 10);
-				_putchar(dig + 48);
-			}
-			_putchar((num % 10) + 48);
-
+			print_number(num);
 			if (j != n)
-			{
-				_putchar(',');
-
-				if ((num + inc) < 10)
-				{
-					_putchar(' ');
-					_putchar(' ');
-				}
-				else if ((num + inc) < 100)
-					_putchar(' ');
-				_putchar(' ');
-			}
+				print_separator(num, inc);
 			num += inc;
 		}
 		_putchar('\n');
 		inc++;
 	}
 }
+
