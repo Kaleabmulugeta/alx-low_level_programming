f60115ef6adc3904007195d0305c0983388f55cd
Kaleab Mulugeta <kalmulmath@gmail.com>
2025-03-09 13:34:13 +0300

    Bug fix on task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,23 +1,36 @@
+int len(char *s)
+{
+	char *cp = s;
+	int leng = 0;
+
+	while (*cp != 0)
+	{
+		leng++;
+		cp++;
+	}
+	return (leng);
+}
+
 /**
  * _strncpy - copies n-bytes from a string to another
  * @dest: Destination
  * @src: source str
  * @n: The number of bytes to copy
  *
- * Return: address of thr destination
+ * Return: address of the destination
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int leng = len(src);
+	int i = 0;
+	int idx = 0;
 
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
-		if (src[i] == '\0')
-		{
-			dest[i] = 0;
-			break;
-		}
-		dest[i] = src[i];
+		dest[i] = src[idx];
+		if (i < leng)
+			idx++;
+		i++;
 	}
 	return (dest);
 }
