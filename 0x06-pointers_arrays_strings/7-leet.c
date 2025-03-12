6994b65e713e0dd5dc07f38c5cdbf6cfd2111437
Kaleab Mulugeta <kalmulmath@gmail.com>
2025-03-12 21:23:35 +0300

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,22 +1,4 @@
 /**
- * len- Length of a string
- * @s: The string
- *
- * Return: The length
- */
-int len(char *s)
-{
-	int length = 0;
-	char *cp = s;
-
-	while (*cp != 0)
-	{
-		cp++;
-		length++;
-	}
-	return (length);
-}
-/**
  * leet- Converts plain text to leet
  * @str: Pointer to the plain text
  *
@@ -24,30 +6,18 @@ int len(char *s)
  */
 char *leet(char *str)
 {
-	int i;
-	int length = len(str);
+	int i, j;
+	char *match = "aAeElLtToO";
+	char *rep = "4433117700";
 
-	for (i = 0; i < length; i++)
+	for (i = 0; str[i] != 0; i++)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
+		for (j = 0; rep[j] != 0; j++)
 		{
-			str[i] = '4';
-		}
-		else if (str[i] == 'e' || str[i] == 'E')
-		{
-			str[i] = '3';
-		}
-		else if (str[i] == 'o' || str[i] == 'O')
-		{
-			str[i] = '0';
-		}
-		else if (str[i] == 't' || str[i] == 'T')
-		{
-			str[i] = '7';
-		}
-		else if (str[i] == 'l' || str[i] == 'L')
-		{
-			str[i] = '1';
+			if (str[i] == match[j])
+			{
+				str[i] = rep[j];
+			}
 		}
 	}
 	return (str);
