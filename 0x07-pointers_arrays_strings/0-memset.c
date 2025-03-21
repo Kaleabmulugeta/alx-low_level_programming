1d91c5e9f1f3f67c9866b4540abcaa1ce261a576
Kaleab Mulugeta <kalmulmath@gmail.com>
2025-03-21 13:39:44 +0300

    compile time errors

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,14 @@
+/**
+ * _memset- Sets value stored in a buffer to char b n times
+ * @s: The buffer
+ * @b: The over-writing character
+ * @n: The number of times to overwrite the buffer
+ *
+ * Return: Pointer to the buffer
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 		s[i] = b;
