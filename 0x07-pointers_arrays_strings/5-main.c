5acfec695dafdc8c92b449b33c6a237e3c439f11
Kaleab Mulugeta <kalmulmath@gmail.com>
2025-03-21 18:05:12 +0300

    Hot Fix

--- 0x07-pointers_arrays_strings/5-main.c
+++ 0x07-pointers_arrays_strings/5-main.c
@@ -8,11 +8,12 @@
  */
 int main(void)
 {
-    char *s = "hello, world";
-    char *f = "world";
+    char *s = "First, solve the problem. Then, write the code.";
+    char *f = "";
     char *t;
 
     t = _strstr(s, f);
-    printf("%s\n", t);
+    if (t != NULL)
+    	printf("%s\n", t);
     return (0);
 }
--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,18 @@
 #define NULL ((void *)0)
+#include <stdio.h>
 
+int len(char *b)
+{
+	char *cp = b;
+	int len = 0;
+
+	while (*cp != 0)
+	{
+		len++;
+		cp++;
+	}
+	return (len);
+}
 /**
  * _strstr- Finds the needle in the haystack
  * @needle: The required substring
@@ -12,8 +25,11 @@ char *_strstr(char *haystack, char *needle)
 	int i, j = 0;
 	char *ret = NULL;
 
+	if (len(needle) == 0)
+		return (haystack);
 	for (i = 0; haystack[i] != 0; i++)
 	{
+		j = 0;
 		if (haystack[i] == needle[j])
 		{
 			ret = &(haystack[i]);
