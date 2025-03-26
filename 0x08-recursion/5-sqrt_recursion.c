59295060a67d9ceb4340c419f7a57753dedbf34c
Kaleab Mulugeta <kalmulmath@gmail.com>
2025-03-26 20:53:02 +0300

    bug

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,23 +1,23 @@
+#include <stdio.h>
 /**
  * guess_sqrt- Finds sqrt of a perfect square
  * @n: The number
- * @guess: First guess
+ * @low: Lower bound
+ * @high: Upper bound
  *
  * Return: sqrt for a perfect square, 0 for non perfects
  */
-int guess_sqrt(int n, int guess)
+int guess_sqrt(int n, int low, int high)
 {
-	if (guess < 0)
+	int mid = low + ((high - low) / 2);
+
+	if (low > high)
 		return (-1);
-	if (guess * guess == n)
-		return (guess);
-	if (guess * guess > n)
-	{
-		if ((guess - 1) * (guess - 1) < n)
-			return (-1);
-		return (guess_sqrt(n, guess - 1));
-	}
-	return (guess_sqrt(n, guess + 1));
+	if (mid * mid == n)
+		return (mid);
+	if (mid * mid > n)
+		return (guess_sqrt(n, low, mid - 1));
+	return (guess_sqrt(n, mid + 1, high));
 }
 /**
  * _sqrt_recursion- Finds sqrt recursively for perfect square numbers
@@ -29,5 +29,5 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (guess_sqrt(n, n / 2));
+	return (guess_sqrt(n, 0, n));
 }
---
+++ 0x08-recursion/test.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	int r = _sqrt_recursion(16777216);
+	printf("%d\n", r);
+}
