38dfbf0c656479e9e9bdd8a6a00f2073e6b6d369
Kaleab Mulugeta <kaleabmulugeta17@hotmail.com>
2025-03-30 17:07:52 +0300

    task 101

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,21 @@
+/**
+ * wildcmp- Compares two strings with support for wildcard char *
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: 1 or 0 accordingly
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+			return (wild_cmp(s1, s2 + 1));
+		return (wild_cmp(s1, s2 + 1) || (*s1 != '\0' && wild_cmp(s1 + 1, s2)));
+	}
+	if (*s1 == *s2)
+		return (wild_cmp(s1 + 1, s2 + 1));
+	return (0);
+}
