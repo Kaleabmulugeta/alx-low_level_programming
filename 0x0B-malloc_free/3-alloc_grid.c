7c76a1addb838ef63ab8bd333e3344f296196756
Kaleab Mulugeta <kaleabmulugeta17@hotmail.com>
2025-04-03 14:25:57 +0300

    memory leak

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -16,12 +16,19 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	arr = (int **)malloc(height * sizeof(int *));
 	if (arr == NULL)
+	{
+		free(arr);
 		return (NULL);
+	}
 	for (i = 0; i < height; i++)
 	{
 		ret = (int *)malloc(width * sizeof(int));
 		if (ret == NULL)
+		{
+			free(ret);
+			free(arr);
 			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 			ret[j] = 0;
 		arr[i] = ret;
